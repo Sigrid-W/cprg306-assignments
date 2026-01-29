8c11bdb89bcd26f0dee63a145e47444ecc7ceca8
Sigrid-W <xin.wen@edu.sait.ca>
2026-01-29 08:05:23 -0700

    add list

--- app/week-3/GroceryItem.js
+++ app/week-3/GroceryItem.js
@@ -3,10 +3,10 @@ export default function Item({ name,
     category,
 }) {
     return (
-        <section className="bg-rose-100 px-4 py-3 mb-3 border-0 rounded-2xl max-w-md mx-auto text-center">
+        <li className="list-none bg-rose-100 px-4 py-3 mb-3 border-0 rounded-2xl max-w-md mx-auto text-center">
             <p className="text-gray-700 font-semibold">{name}</p>
             <p className="text-gray-700">Quantity: {quantity}</p>
             <p className="text-gray-700">Category: {category}</p>
-        </section>
+        </li>
     );
 }
