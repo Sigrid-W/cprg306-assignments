75999073771ee95278a7d3fb01daccca00ac9322
Sigrid-W <xin.wen@edu.sait.ca>
2026-01-29 17:11:47 -0700

    update nav page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,35 @@
 import Link from 'next/link'
+const navItems = [
+  {
+    title: "Go to Week 2",
+    href: "week-2",
+  },
+  {
+    title: "Go to Week 3",
+    href: "week-3",
+  },
+]
 
 export default function Page() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <div className="flex items-center gap-3">
-      <Link href="http://localhost:3000/week-2">Go to Week 2</Link>
-      <span>→</span>
-      <Link href="http://localhost:3000/week-3">Go to Week 3</Link>
-    </div>
+
+      <nav className="flex items-center gap-3">
+        {navItems.map((item, index) => (
+          <span key={item.href} className="flex items-center gap-3">
+            <Link
+              href={item.href}
+              className="hover:text-blue-600 duration-300 ease-linear"
+            >
+              {item.title}
+            </Link>
+
+            {index < navItems.length - 1 && <span>→</span>}
+          </span>
+        ))}
+      </nav>
+
     </main>
   );
 }
