9b8723ace9b9e2ed713f1d77560de9db1b927996
Sigrid-W <xin.wen@edu.sait.ca>
2026-02-06 20:49:05 -0700

    change title colour

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list";
 export default function Page() {
     return (
         <main>
-            <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">Shopping List</h1>
+            <h1 className="text-2xl font-bold text-green-950 mb-4 text-center">Shopping List</h1>
             <ItemList/>
         </main>
     );
